8b2c4dfe8eb1dac2ac71131779ea6b6f88deedf4
Zhaniyau <zhaia_99@mail.ru>
2024-01-12 20:34:38 +0600

    Version 2

--- src/Yrok_12_01_2024/NewFile.java
+++ src/Yrok_12_01_2024/NewFile.java
@@ -1,2 +1,7 @@
-package Yrok_12_01_2024;public class NewFile {
+package Yrok_12_01_2024;
+
+public class NewFile {
+    public static void main(String[] args) {
+        System.out.println("TEST");
+    }
 }
